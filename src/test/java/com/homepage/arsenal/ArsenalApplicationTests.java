0bd2e533825b6964501e1214c56d471c15dc5321
noturavrigk <ksm92038386@gmail.com>
2025-02-10 00:51:04 +0900

    chore: first commit for push to github repo

---
+++ src/test/java/com/homepage/arsenal/ArsenalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homepage.arsenal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsenalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
